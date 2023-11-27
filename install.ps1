385ccd3b9be9962e1c0f89f1c02b406b839fdd55
Hendrik du Toit <hendrik@brightedge.co.za>
2023-11-27 02:03:51 +0200

    Setup structure and statutory

---
+++ install.ps1
@@ -0,0 +1,3 @@
+if (Test-Path -Path $env:PROJECT_DIR\pyproject.toml) {pip install --no-cache-dir -e .[dev]}
+pip install -e D:\Dropbox\Projects\BEE\displayfx
+pip install -e D:\Dropbox\Projects\BEE\beetools
--- tests/test_fixdate.py
+++ tests/test_fixdate.py
@@ -1,40 +1,5 @@
-'''Testing fixdate__init__()'''
-
-from pathlib import Path
-from beetools.beearchiver import Archiver
-import fixdate
-
-
-_PROJ_DESC = __doc__.split('\n')[0]
-_PROJ_PATH = Path(__file__)
-
-
-def project_desc():
-    return _PROJ_DESC
-
-
-b_tls = Archiver(_PROJ_DESC, _PROJ_PATH)
-
-
-class TestFixDate:
-    def test__init__(self, env_setup_self_destruct):
-        """Assert class __init__"""
-        env_setup = env_setup_self_destruct
-        t_fixdate = fixdate.FixDate("FixDate", env_setup.dir)
-
-        assert t_fixdate.success
-        pass
-
-    def test_method_1(self, env_setup_self_destruct):
-        """Assert class __init__"""
-        env_setup = env_setup_self_destruct
-        t_fixdate = fixdate.FixDate("FixDate", env_setup.dir)
-
-        assert t_fixdate.method_1("THis is a test message for Method_1")
-        pass
-
-    def test_do_examples(self):
-        fixdate.do_examples()
-
-
-del b_tls
+class TestCsvWrpr:
+    def test_(self):
+        x = 1
+        y = 1
+        assert x == y
